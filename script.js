1b9429eab588d3b2e245811866636baaeaaa05c2
unknown <davisoares1999@hotmail.com>
2022-01-24 18:19:16 -0300

    upando desafio 01 vnw

---
+++ script.js
@@ -0,0 +1,32 @@
+/* Questão 1 */
+
+let nome = "Davi"
+
+/* Questão 2 */
+
+let idade = 22
+
+/* Questão 3 */
+
+let anoAtual = 2022
+let diferenca
+
+diferenca = anoAtual - idade
+console.log(diferenca)
+
+/* Questão 4 */
+
+let soma = anoAtual + idade
+
+console.log(soma)
+
+/* Questão 5 */
+
+console.log("Estou mexendo no console.log")
+
+/* Questão 6 */
+
+let quartaFeira = true
+
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
